8d351e9f1887fbfac2834de50d85b51d4a3b7cb0
Hendra Dwi Saputra <hendra@usa.com>
2021-02-02 02:09:22 +0700

    API Kabupaten (filter by kode propinsi)

--- daerah/urls.py
+++ daerah/urls.py
@@ -4,4 +4,5 @@ from daerah import views
 
 urlpatterns = [
     path('propinsi/', views.Propinsi.as_view(), name='Propinsi'),
+    path('kabupaten/<str:kode>/', views.Kabupaten.as_view(), name='Kabupaten'),
 ]
--- daerah/views.py
+++ daerah/views.py
@@ -8,3 +8,13 @@ from daerah.serializers import DaerahSerializer
 class Propinsi(ListAPIView):
     queryset = Daerah.objects.annotate(length=Length('kode')).filter(length=2)
     serializer_class = DaerahSerializer
+
+
+class Kabupaten(ListAPIView):
+    lookup_field = 'kode'
+    serializer_class = DaerahSerializer
+
+    def get_queryset(self):
+        kode = self.kwargs.get('kode')
+        queryset = Daerah.objects.annotate(length=Length('kode')).filter(length=5, kode__startswith=kode)
+        return queryset
