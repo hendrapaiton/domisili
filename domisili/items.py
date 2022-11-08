7bb4597fa99a214b043a5980d6f4d3caec730b5a
Hendra Dwi Saputra <hendra@usa.com>
2022-11-08 17:08:16 +0700

    Model item for data field

--- domisili/items.py
+++ domisili/items.py
@@ -1,12 +1,6 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
-
 import scrapy
 
 
 class DomisiliItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    code = scrapy.Field()
+    name = scrapy.Field()
